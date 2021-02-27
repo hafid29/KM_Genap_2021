1dcb4c5aa60acb6095d336ba06003a348a44fde7
hafid29 <hafiwibisana29@gmail.com9>
2021-02-27 17:45:23 +0700

    pertemuan 2

---
+++ Pertemuan 2/Panel-jack-diamonds/Panel-jack-diamonds/Assets/ArrangeActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ArrangeActions : MonoBehaviour
+{
+    private RectTransform panelRectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+    public void MoveDownOne()
+    {
+        print("(before change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex =
+        panelRectTransform.GetSiblingIndex();
+        panelRectTransform.GetSiblingIndex(currentSiblingIndex - 1);
+        print("(after change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+    public void MoveUpOne()
+    {
+        print("(before change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex =
+        panelRectTransform.GetSiblingIndex();
+        panelRectTransform.GetSiblingIndex(currentSiblingIndex + 1);
+        print("(after change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+}
---
+++ Pertemuan 2/buttonUI/Assets/MenuAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAction : MonoBehaviour
+{ 
+    public void MENU_ACTION_GotoPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
