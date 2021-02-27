46002186a2f10ab2db4cf8d7f3a03cd6216c13c0
hafid29 <hafiwibisana29@gmail.com9>
2021-02-27 17:42:05 +0700

    pertemuan 1

---
+++ Pertemuan 1/Hello_World/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public string myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hello World");
+        //Debug.Log("myText");
+    }
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        GUILayout.Label(myText);
+        GUILayout.EndArea();
+    }
+}
