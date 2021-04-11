d261ec85210686f35b6543e09320a076c7236fc3
hafid29 <hafidwibisana29@gmail.com>
2021-04-11 11:42:58 +0700

    modul 8

---
+++ Pertemuan 8/Tugas/Assets/Script/UpdateProbe.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+public class UpdateProbe : MonoBehaviour
+{
+    private ReflectionProbe probe; void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>(); probe.RenderProbe();
+    }
+    public void RefreshProbe()
+    {
+        probe.RenderProbe();
+    }
+}
