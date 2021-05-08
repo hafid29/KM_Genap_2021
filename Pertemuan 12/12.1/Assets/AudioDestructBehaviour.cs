cfe617ad811d00cf2e61b746e16dfe39340446fe
hafid29 <hafidwibisana29@gmail.com>
2021-05-08 23:12:38 +0700

    modul12

---
+++ Pertemuan 12/12.1/Assets/AudioDestructBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+public class AudioDestructBehaviour : MonoBehaviour
+{
+private AudioSource audioSource; void Start()
+{
+audioSource = GetComponent<AudioSource>();
+}
+private void Update()
+{
+if (!audioSource.isPlaying) Destroy(gameObject);
+}
+}
---
+++ Pertemuan 12/12.3/Assets/SnapshotTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Audio; using System.Collections;
+public class SnapshotTrigger : MonoBehaviour
+{
+public AudioMixerSnapshot snapshot; public float crossfade;
+private void OnTriggerEnter(Collider other)
+{
+snapshot.TransitionTo(crossfade);
+}
+}
