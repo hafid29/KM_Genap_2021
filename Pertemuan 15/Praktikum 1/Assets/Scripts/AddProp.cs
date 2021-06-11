8e16543bfad91cb1dd5a2b940b58884259199030
hafid29 <hafidwibisana29@gmail.com>
2021-06-11 19:38:57 +0700

    praktikum 1

---
+++ Pertemuan 15/Praktikum 1/Assets/Scripts/AddProp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddProp : MonoBehaviour
+{
+    public GameObject prop;
+    public Transform targetBone;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset;
+    public bool destroyTrigger = true;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (targetBone.IsChildOf(collision.transform))
+        {
+            bool checkProp = false;
+            foreach(Transform child in targetBone)
+            {
+                if (child.name == prop.name)
+                    checkProp = true;
+            }
+            if (!checkProp)
+            {
+                GameObject newprop;
+                newprop = Instantiate(prop, targetBone.position, targetBone.rotation) as GameObject;
+                newprop.name = prop.name;
+                newprop.transform.parent = targetBone;
+                newprop.transform.localPosition += positionOffset;
+                newprop.transform.localEulerAngles += rotationOffset;
+                if (destroyTrigger)
+                    Destroy(gameObject);
+            }
+        }
+    }
+}
