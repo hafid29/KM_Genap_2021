2a9f1eb1c35b5ce3707a9396e85ae76dbdcfbaa1
hafid29 <hafidwibisana29@gmail.com>
2021-06-11 19:41:21 +0700

    praktikum 3

---
+++ Pertemuan 15/Praktikum 3/Assets/Scripts/Landmine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Landmine : MonoBehaviour
+{
+    public float range = 2f;
+    public float force = 2f;
+    public float up = 4f;
+    private bool active = true;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player" && active)
+        {
+            active = false;
+            StartCoroutine(Reactivate());
+            collision.gameObject.GetComponent<RagdollCharacter>().ActivateRagdoll();
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, range);
+
+            foreach(Collider hit in colliders)
+            {
+                if (hit.GetComponent<Rigidbody>())
+                    hit.GetComponent<Rigidbody>().AddExplosionForce(force, explosionPos, range, up);
+            }
+        }  
+    }
+
+    IEnumerator Reactivate()
+    {
+        yield return new WaitForSeconds(5);
+        active =  true;
+    }
+}
---
+++ Pertemuan 15/Praktikum 3/Assets/Scripts/RagdollCharacter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RagdollCharacter : MonoBehaviour
+{
+
+    void Start()
+    {
+        DeactivateRagdoll();
+    }
+
+    public void ActivateRagdoll()
+    {
+        gameObject.GetComponent<CharacterController>().enabled = false;
+        gameObject.GetComponent<BasicController>().enabled = false;
+        gameObject.GetComponent<Animator>().enabled = false;
+
+        foreach (Rigidbody bone in GetComponentsInChildren<Rigidbody>())
+        {
+            bone.isKinematic = false;
+            bone.detectCollisions = true;
+        }
+
+        foreach (Collider col in GetComponentsInChildren<Collider>())
+        {
+            col.enabled = true;
+        }
+
+        StartCoroutine(Restore());
+    }
+
+    public void DeactivateRagdoll()
+    {
+        gameObject.GetComponent<BasicController>().enabled = true;
+        gameObject.GetComponent<Animator>().enabled = true;
+        transform.position = GameObject.Find("Spawnpoint").transform.position;
+        transform.rotation = GameObject.Find("Spawnpoint").transform.rotation;
+
+        foreach (Rigidbody bone in GetComponentsInChildren<Rigidbody>())
+        {
+            bone.isKinematic = true;
+            bone.detectCollisions = false;
+        }
+
+        foreach (CharacterJoint joint in GetComponentsInChildren<CharacterJoint>())
+        {
+            joint.enableProjection = false;
+        }
+
+        foreach (Collider col in GetComponentsInChildren<Collider>())
+        {
+            col.enabled = true;
+        }
+
+        gameObject.GetComponent<CharacterController>().enabled = true;
+    }
+
+        IEnumerator Restore()
+        {
+            yield return new WaitForSeconds(5);
+            DeactivateRagdoll();
+        }
+    }
